5d9fa6abf0f2a1c7e09c9cf00287e039feefa38a
Guilherme Santos <gsantos.comp@gmail.com>
2019-03-27 13:13:03 -0300

    Comparação de indivíduos durante a geração

--- population.py
+++ population.py
@@ -1,6 +1,5 @@
 from individual import Individual
 
-
 class Population(object):
 
     def __init__(self, quantity: int):
@@ -9,24 +8,25 @@ class Population(object):
 
     # Verifica se existem indivíduos com genes idênticos
     # dentro da população e retorna uma lista com os indivíduos repetidos
-    def compare_individuals(self):
-        aux_list = self.individuals
-        repeated_genes = []
+    def equal_individuals(self, individual: Individual):
+        for x in self.individuals:
+            if x.get_genes() == individual.get_genes()
+                return True
+        return False
 
-        for i in range(7):
-            for j in range(i+1, 8):
-                if aux_list[i].get_genes() == self.individuals[j].get_genes():
-                    repeated_genes.append(j)
-
-        return repeated_genes
+    def change_individuals(self, individuals: list):
+        for x in individuals:
+            self.individuals[x] = Individual()
 
     # Gera a quantidade de indivíduos necessária determinada para a população
     def generate_individuals(self):
-        for i in range(self.quantity):
-            self.individuals.append(Individual())
+        for i in range(self.quantity)
+            actual = Individual()
+            if not equal_individuals(actual)
+                self.individuals.append(actual)
+            else
+                i -= 1
 
     def get_individuals(self):
         for x in self.individuals:
             print(x.get_genes())
-
-
