ceb6ffdfe11a141afb2c98c1c2bc7bb367ec8378
Guilherme Santos <gsantos.comp@gmail.com>
2019-03-28 01:19:07 -0300

    Verificação sobre a restrição de peso das sacolas

--- individual.py
+++ individual.py
@@ -12,5 +12,21 @@ class Individual(object):
         else:
             self.genes = [randint(0, 1) for x in range(5)]
 
-    def get_genes(self):
+    def __getitem__(self, gene):
+        return self.genes[gene]
+
+    def get_all_genes(self):
         return self.genes
+
+    # Verifica se a restrição é obedecida
+    # A soma dos pesos das sacolas não deve ultrapassar 90kg
+    def check_viability(self):
+        weights = [30, 20, 50, 35, 15]
+        result = 0
+
+        for i in range(5):
+            result += weights[i] * self[i]
+
+        if result > 90:
+            return False
+        return True
--- population.py
+++ population.py
@@ -8,27 +8,26 @@ class Population(object):
         self.quantity = quantity
         self.individuals = []
 
+    def __getitem__(self, individual):
+        return self.individuals[individual]
+
     # Verifica se existem indivíduos com genes idênticos
     # dentro da população e retorna uma lista com os indivíduos repetidos
     def equal_individuals(self, individual: Individual):
         for x in self.individuals:
-            if x.get_genes() == individual.get_genes():
+            if x.get_all_genes() == individual.get_all_genes():
                 return True
         return False
-
-    def change_individuals(self, individuals: list):
-        for x in individuals:
-            self.individuals[x] = Individual()
 
     # Gera a quantidade de indivíduos necessária determinada para a população
     def generate_individuals(self):
         i = 0
         while i < self.quantity:
             actual = Individual()
-            if not self.equal_individuals(actual):
+            if (actual.check_viability()) and (not self.equal_individuals(actual)):
                 self.individuals.append(actual)
                 i += 1
 
     def get_individuals(self):
         for x in self.individuals:
-            print(x.get_genes())
+            print(x.get_all_genes())
